d484d86760978a376615d9bd1c935311fafc9d4b
Hayder Abu AlHummos <greekrifleopens@gmail.com>
2023-10-30 19:39:27 +0300

    doggies

--- Components/DogList/DogList.jsx
+++ Components/DogList/DogList.jsx
@@ -1,16 +1,13 @@
+import React from "react";
 
-
-export default function ShoppingList({ dogsList = [] }) {
+export default function DogList({ dogsList }) {
   return (
     <div className="shopping-cart">
       <h3 style={{ textAlign: "center" }}>
-        Cute Dogs
-        <span role="img">
-          🐶
-        </span>
+        Cute Dogs <span role="img">🐶</span>
       </h3>
-      {dogsList.map((dog) => (
-        <img src={dog} alt="" />
+      {dogsList.map((dog, index) => (
+        <img key={index} src={dog} alt={`Dog ${index}`} />
       ))}
     </div>
   );
--- Components/Form/Form.jsx
+++ Components/Form/Form.jsx
@@ -1,10 +1,25 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
-
-export default function Form({ setNumberOfDogs }) {
-  // How can we manage the number of dogs?
-
+const Form = ({ setDogsData }) => {
   const [inputText, setInputText] = useState("");
+
+  const handleButtonClick = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(
+        `https://dog.ceo/api/breeds/image/random/${inputText}`
+      );
+      const data = await response.json();
+      if (data && data.status === "success") {
+        setDogsData(data.message);
+      } else {
+        console.error("Error fetching dog data:", data);
+      }
+    } catch (error) {
+      console.error("Error fetching dog data:", error);
+    }
+  };
+
   return (
     <form>
       <h3>Number of cute dogs is infinite!</h3>
@@ -12,19 +27,14 @@ export default function Form({ setNumberOfDogs }) {
         <label>Insert number of cute dogs</label>
         <input
           value={inputText}
-          onChange={(e) => {
-            setInputText(e.target.value);
-          }}
-          type="text"
+          onChange={(e) => setInputText(e.target.value)}
+          type="number"
           className="form-control"
         />
       </div>
       <div className="d-grid">
         <button
-          onClick={(e) => {
-            e.preventDefault();
-            // When we click on the WOOF! button, what happens?
-          }}
+          onClick={handleButtonClick}
           type="submit"
           className="btn btn-primary"
         >
@@ -33,4 +43,6 @@ export default function Form({ setNumberOfDogs }) {
       </div>
     </form>
   );
-}
+};
+
+export default Form;
